75f916c9ae5cd2774b8a70559229434d361a63ac
Khushi Kumari <muskankumari00999.mk@gmail.com>
2025-09-04 13:05:19 +0530

    Learn About Collection Binding

---
+++ DotNet/MAUI/MAUI_Test_App/MAUI_Test_App/MVVM/Models/Person.cs
@@ -0,0 +1,10 @@
+﻿namespace MAUI_Test_App.MVVM.Models
+{
+     public class Person
+     {
+          public string Name { get; set; }
+          public int Age { get; set; }
+          public string City { get; set; }
+          public string Married { get; set; }
+     }
+}
---
+++ DotNet/MAUI/MAUI_Test_App/MAUI_Test_App/MVVM/ViewModels/ConverterClass.cs
@@ -0,0 +1,29 @@
+﻿using System.Globalization;
+
+namespace MAUI_Test_App.MVVM.ViewModels
+{
+     public class ConverterClass
+ : IValueConverter
+     {
+          public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+          {
+               string married = value.ToString();
+               if (married == "Married")
+               {
+                    return true;
+               }
+               else
+               {
+                    return false;
+               }
+          }
+
+          public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+          {
+               if (value is bool isChecked && isChecked)
+                    return "Married";
+               else
+                    return "Single";
+          }
+     }
+}
