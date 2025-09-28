93cb1908361471d65dbed974a662b5a734b2f123
MuskanKumariMK <muskankumari00999.mk@gmail.com>
2025-09-28 13:01:10 +0530

    Add Jwt Auth

---
+++ DotNet/JWTAuth/JWTAuth/Data/UserRepository.cs
@@ -0,0 +1,23 @@
+﻿
+using JWTAuth.Models;
+
+namespace JWTAuth.Data
+{
+    public class UserRepository : IUserRepository
+    {
+        private readonly UserDbContext _usercontext;
+        public UserRepository(UserDbContext _usercontext)
+        {
+                
+        }  public  User Create(User user)
+        {
+                _usercontext.Users.Add(user);
+             user.Id =   _usercontext.SaveChanges();
+            return user;
+        }
+        public  User GetEmail(User email)
+        {
+            return _usercontext.Users.FirstOrDefault(u => u.Email == email);
+        }
+    }
+}
