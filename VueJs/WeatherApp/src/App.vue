10addbca254e37e63de971fc83dd1e42998a96e4
MuskanKumariMK <muskankumari00999.mk@gmail.com>
2025-09-29 10:11:32 +0530

    Weather App

---
+++ VueJs/WeatherApp/src/App.vue
@@ -0,0 +1,15 @@
+<script setup></script>
+
+<template>
+  <div class="flex flex-col min-h-screen">
+    <!-- Main Content -->
+    <main class="flex-1 p-4 bg-gray-50">
+      <router-view />
+    </main>
+
+    <!-- Footer -->
+    <footer class="p-4 text-center text-gray-500 bg-gray-200">
+      © 2025 My Vue App
+    </footer>
+  </div>
+</template>
