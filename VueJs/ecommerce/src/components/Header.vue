ec69a182aff337e9e01aafd1b61bcabbec31bf0c
Muskan Kumari <muskankumari00999.mk@gmail.com>
2025-09-29 11:37:38 +0530

    Ecoomerce

---
+++ VueJs/ecommerce/src/components/Header.vue
@@ -0,0 +1,18 @@
+<template>
+    <header class="sticky top-0 z-50 transition-all duration-300">
+<section>
+
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+<div class="flex flex-col md:flex-row item-center justify-between gap-3 md:gap-6"></div>
+    </div>
+</section>    
+
+    </header>
+</template>
+<script setup>
+export default {
+  name: 'Header',
+};
+</script>
+
+<style></style>
