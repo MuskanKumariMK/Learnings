be290a391900a1eb9232d01daee0444c147add17
MuskanKumariMK <muskankumari00999.mk@gmail.com>
2026-08-13 11:43:36 +0530

    Data Templates Learned

---
+++ DotNet/MAUI/MAUI_Test_App/MAUI_Test_App/Resources/Styles/ClassResourceDictionary.xaml.cs
@@ -0,0 +1,9 @@
+namespace MAUI_Test_App.Resources.Styles;
+
+public partial class ClassResourceDictionary : ResourceDictionary
+{
+    public ClassResourceDictionary()
+    {
+        InitializeComponent();
+    }
+}
---
+++ DotNet/MAUI/MAUI_Test_App/MAUI_Test_App/Selectors/ClothDataTemplateSelector.cs
@@ -0,0 +1,18 @@
+﻿using MAUI_Test_App.MVVM.Models;
+
+namespace MAUI_Test_App.Selectors
+{
+    public class ClothDataTemplateSelector : DataTemplateSelector
+    {
+        protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+        {
+            var cloth = item as Cloth;
+            if (!cloth.HasOffer)
+            {
+                Application.Current.Resources.TryGetValue("ClothResource", out var ClothResource);
+                return ClothResource as DataTemplate;
+            }
+            return new DataTemplate();
+        }
+    }
+}
